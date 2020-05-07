daaecda564fd3a66708c015ecc4a6d762d7827af
Lovepreet Singh <lovepreet013hundal@gmail.com>
2020-05-07 11:34:31 +0530

    run_docker.sh script updated

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag flaskapp:1.0 .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker rm flaskapp:1.0
+docker run --publish 8000:80 --detach --name fa flaskapp:1.0
