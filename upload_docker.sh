424ed46622a4d268ad0d9d9a76dc9648d24b0a01
Lovepreet Singh <lovepreet013hundal@gmail.com>
2020-05-12 09:38:37 +0530

    upload_docker.sh script updated

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=lovepreet013/flaskapp
 
 # Step 2:  
 # Authenticate & tag
+docker login
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker tag flaskapp:1.0 lovepreet013/flaskapp
+docker push lovepreet013/flaskapp
